f83f6dbfae66ea4ec0e20a4e83a571a12efbbdd0
agriso <albert.griso.mendez@gmail.com>
2021-03-27 00:23:47 +0100

    mdi Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template lang="pug">
 .container.home
   img(alt="Vue logo" src="../assets/logo.png")
+  p
+    Icon(type="mdi" :path="path")
+    Icon(type="mdi" :path="path" color="red")
   HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import { mdiCheckCircle  } from '@mdi/js';
+import Icon from '@/components/Icon/Icon.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+    Icon
   },
+  data() {
+    return { 
+      path: mdiCheckCircle  
+    }
+  }
 });
 </script>
 
